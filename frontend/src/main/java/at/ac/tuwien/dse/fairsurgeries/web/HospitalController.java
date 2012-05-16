f99415924b6dd133a84a41b725fd5f6391f90e87
Matthias Tretter <matthias.tretter@gmail.com>
2012-05-16 19:09:58 +0200

    Fixed maven dependency to domain project

--- frontend/src/main/java/at/ac/tuwien/dse/fairsurgeries/web/HospitalController.java
+++ frontend/src/main/java/at/ac/tuwien/dse/fairsurgeries/web/HospitalController.java
@@ -1,12 +1,25 @@
 package at.ac.tuwien.dse.fairsurgeries.web;
 
-import at.ac.tuwien.dse.fairsurgeries.domain.Hospital;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.roo.addon.web.mvc.controller.scaffold.RooWebScaffold;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import at.ac.tuwien.dse.fairsurgeries.domain.Hospital;
 
 @RequestMapping("/hospitals")
 @Controller
 @RooWebScaffold(path = "hospitals", formBackingObject = Hospital.class)
 public class HospitalController {
+	
+	@RequestMapping
+	public void get(HttpServletResponse response) throws Exception {
+		PrintWriter out = response.getWriter();
+		
+		out.println("Gerhard as a Service");
+	}
 }
+
