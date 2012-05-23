13c10f5aa30ce73ab6765402b145f77bd3e0ec7f
Gerhard Schraml <g.schraml@gmx.at>
2012-05-23 18:24:05 +0200

    rendering output to jspx

--- frontend/src/main/java/at/ac/tuwien/dse/fairsurgeries/web/HospitalController.java
+++ frontend/src/main/java/at/ac/tuwien/dse/fairsurgeries/web/HospitalController.java
@@ -6,7 +6,11 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.roo.addon.web.mvc.controller.scaffold.RooWebScaffold;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import at.ac.tuwien.dse.fairsurgeries.domain.Hospital;
 
@@ -15,11 +19,18 @@ import at.ac.tuwien.dse.fairsurgeries.domain.Hospital;
 @RooWebScaffold(path = "hospitals", formBackingObject = Hospital.class)
 public class HospitalController {
 	
-	@RequestMapping
-	public void get(HttpServletResponse response) throws Exception {
+	@RequestMapping(value = "/matthias", produces = "application/json")
+	public void matthias(HttpServletResponse response) throws Exception {
 		PrintWriter out = response.getWriter();
 		
-		out.println("Gerhard as a Service");
+		out.println("Matthias as a Service");
 	}
+	
+	@RequestMapping(value = "/gerhard/{message}", produces = "text/html")
+    public String gerhard(@PathVariable String message, Model uiModel) {
+        uiModel.addAttribute("myFreakinMessage", new String(message));
+        uiModel.addAttribute("myFreakinTitle", new String("I can output whatever i want"));
+        return "hospitals/gerhard";
+    }
 }
 
