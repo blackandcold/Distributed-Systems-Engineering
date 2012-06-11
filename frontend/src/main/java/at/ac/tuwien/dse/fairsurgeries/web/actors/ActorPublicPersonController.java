71fd49118062f781ac30e5058d0a93bcd3dc47de
Gerhard Schraml <g.schraml@gmx.at>
2012-06-11 21:22:56 +0200

    fooling around with ui views... wir sind sowas von am arsch

---
+++ frontend/src/main/java/at/ac/tuwien/dse/fairsurgeries/web/actors/ActorPublicPersonController.java
@@ -0,0 +1,40 @@
+package at.ac.tuwien.dse.fairsurgeries.web.actors;
+
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import at.ac.tuwien.dse.fairsurgeries.general.Constants;
+import at.ac.tuwien.dse.fairsurgeries.service.LogEntryService;
+import at.ac.tuwien.dse.fairsurgeries.service.OPSlotService;
+
+@Controller
+@RequestMapping("/actors/public")
+public class ActorPublicPersonController {
+	
+	@Autowired
+	private OPSlotService opSlotService;
+	@Autowired
+	private LogEntryService logEntryService;
+
+	@RequestMapping(value="/slots", method = RequestMethod.GET, produces = "text/html")
+	public String listSlots(Model uiModel) {
+		logEntryService.log(Constants.Component.Frontend.toString(), "Starting ActorPublicPersonController . listSlots()");
+		uiModel.addAttribute("heading", "List all feckin Slots");
+		uiModel.addAttribute("slots", opSlotService.findAllOPSlots());
+		return "actors/public/slots";
+	}
+	
+	/*@RequestMapping(value="/slots", method = RequestMethod.GET, produces = "text/html")
+	public void listSlots(HttpServletResponse response) throws Exception {
+		PrintWriter out = response.getWriter();
+
+		out.println("feckin shit");
+	}*/
+}
