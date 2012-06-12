b45b7d0d5356991290c71ff5ae754c9ddc2ecb08
Gerhard Schraml <g.schraml@gmx.at>
2012-06-12 21:54:43 +0200

    op slot filtering 2

--- frontend/src/main/java/at/ac/tuwien/dse/fairsurgeries/web/actors/ActorPublicPersonController.java
+++ frontend/src/main/java/at/ac/tuwien/dse/fairsurgeries/web/actors/ActorPublicPersonController.java
@@ -35,8 +35,9 @@ public class ActorPublicPersonController {
 	
 	@RequestMapping(value="/slots", method = RequestMethod.POST, produces = "text/html")
 	public String listFilteredSlots(@ModelAttribute OPSlot opSlot, Model uiModel) {
-		logEntryService.log(Constants.Component.Frontend.toString(), "Starting ActorPublicPersonController . listFilteredSlots()");
-		uiModel.addAttribute("opSlotExample", opSlotService.findByExample(opSlot));
+		logEntryService.log(Constants.Component.Frontend.toString(), "Starting ActorPublicPersonController . listFilteredSlots() example=" + opSlot);
+		//uiModel.addAttribute("opSlotExample", opSlotService.findByExample(opSlot));
+		uiModel.addAttribute("opSlots", opSlotService.findAllOPSlots());
 		uiModel.addAttribute("opSlotExample", opSlot);
 		return "actors/public/slots";
 	}
