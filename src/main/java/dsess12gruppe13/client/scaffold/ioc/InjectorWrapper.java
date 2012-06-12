e43b9fee07cee5439bb70ba374d8b0c0ac7e09b6
Johannes Kurz <blackandcold@new-host-2.home>
2012-06-12 12:07:31 +0200

    removed old files from my local repository

--- src/main/java/dsess12gruppe13/client/scaffold/ioc/InjectorWrapper.java
+++
@@ -1,6 +0,0 @@
-package dsess12gruppe13.client.scaffold.ioc;
-
-public interface InjectorWrapper {
-
-	ScaffoldInjector getInjector();
-}
--- src/main/java/dsess12gruppe13/client/scaffold/ioc/ScaffoldInjector.java
+++
@@ -1,9 +0,0 @@
-package dsess12gruppe13.client.scaffold.ioc;
-
-import dsess12gruppe13.client.scaffold.ScaffoldApp;
-import com.google.gwt.inject.client.Ginjector;
-
-public interface ScaffoldInjector extends Ginjector {
-
-	ScaffoldApp getScaffoldApp();
-}
