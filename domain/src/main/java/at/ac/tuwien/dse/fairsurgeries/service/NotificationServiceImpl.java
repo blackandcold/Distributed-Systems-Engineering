2f4581a07f2ace085ed7fd3f3fc8c3636dfd4168
Gerhard Schraml <g.schraml@gmx.at>
2012-06-14 11:40:04 +0200

    hospital view notifications

--- domain/src/main/java/at/ac/tuwien/dse/fairsurgeries/service/NotificationServiceImpl.java
+++ domain/src/main/java/at/ac/tuwien/dse/fairsurgeries/service/NotificationServiceImpl.java
@@ -1,5 +1,15 @@
 package at.ac.tuwien.dse.fairsurgeries.service;
 
+import java.util.List;
+
+import at.ac.tuwien.dse.fairsurgeries.domain.Hospital;
+import at.ac.tuwien.dse.fairsurgeries.domain.Notification;
+
 
 public class NotificationServiceImpl implements NotificationService {
+	//public List<Notification> findByHospital(Hospital hospital) {
+	//	List<Notification> allNotifications = notificationRepository.findAllNotifications();
+	//	return allNotifications;
+	//}
+	// FUCK ASPECTS ARENT WORKING
 }
