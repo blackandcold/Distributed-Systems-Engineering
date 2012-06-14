fa339f68bb2986b51d9ee3ab2e645362c1712ca5
Matthias Tretter <matthias.tretter@gmail.com>
2012-06-14 12:18:30 +0200

    Use public/slots to display slots of patient with findByExample

--- frontend/src/main/java/at/ac/tuwien/dse/fairsurgeries/web/actors/ActorPatientController.java
+++ frontend/src/main/java/at/ac/tuwien/dse/fairsurgeries/web/actors/ActorPatientController.java
@@ -1,8 +1,11 @@
 package at.ac.tuwien.dse.fairsurgeries.web.actors;
 
 import java.math.BigInteger;
+import java.util.Arrays;
 
+import org.joda.time.format.DateTimeFormat;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -10,7 +13,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import at.ac.tuwien.dse.fairsurgeries.domain.OPSlot;
 import at.ac.tuwien.dse.fairsurgeries.domain.Patient;
+import at.ac.tuwien.dse.fairsurgeries.domain.SurgeryType;
 import at.ac.tuwien.dse.fairsurgeries.general.Constants;
 import at.ac.tuwien.dse.fairsurgeries.service.DoctorService;
 import at.ac.tuwien.dse.fairsurgeries.service.HospitalService;
@@ -53,15 +58,21 @@ public class ActorPatientController {
 	public String viewSlots(@ModelAttribute Patient patient, Model uiModel) {
 		logEntryService.log(Constants.Component.Frontend.toString(), "Starting ActorPatient . viewSlots() for patient: " + patient);
 		logEntryService.log(Constants.Component.Frontend.toString(), "uiModel: " + uiModel);
-		
-		/*if (patient != null) {
-			uiModel.addAttribute("heading", "Slots of Patient " + patient.getLastName());
-			uiModel.addAttribute("slots", patient.getOpSlots());
 
-			return "actors/public/slots";
-		}*/
+		OPSlot filterExample = new OPSlot();
+		filterExample.setPatient(patient);
 		
-		return "actors/patient/viewslots";
+		uiModel.addAttribute("opSlots", opSlotService.findByPatient(patient));
+		uiModel.addAttribute("surgeryTypes", Arrays.asList(SurgeryType.values()));
+		uiModel.addAttribute("hospitals", hospitalService.findAllHospitals());
+		uiModel.addAttribute("doctors", doctorService.findAllDoctors());
+		uiModel.addAttribute("patients", Arrays.asList(patient));
+		uiModel.addAttribute("OPSlot_datefrom_date_format", DateTimeFormat.patternForStyle("MS", LocaleContextHolder.getLocale()));
+		uiModel.addAttribute("OPSlot_dateto_date_format", DateTimeFormat.patternForStyle("MS", LocaleContextHolder.getLocale()));
+		uiModel.addAttribute("opSlotExample", filterExample);
+		uiModel.addAttribute("showPatientName", false);
+
+		return "actors/public/slots";
 	}
 	
 	@RequestMapping(value = "/viewnotifications", method = RequestMethod.POST)
