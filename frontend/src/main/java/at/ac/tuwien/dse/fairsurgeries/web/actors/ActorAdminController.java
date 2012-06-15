5e10be4a7cd63fc87fff5cc541b50b64cad0d34d
Gerhard Schraml <g.schraml@gmx.at>
2012-06-15 07:49:21 +0200

    adapted filtering tag for patient filtering and applied to admin op slot list

--- frontend/src/main/java/at/ac/tuwien/dse/fairsurgeries/web/actors/ActorAdminController.java
+++ frontend/src/main/java/at/ac/tuwien/dse/fairsurgeries/web/actors/ActorAdminController.java
@@ -2,6 +2,8 @@ package at.ac.tuwien.dse.fairsurgeries.web.actors;
 
 import java.util.Arrays;
 
+import javax.servlet.ServletRequest;
+
 import org.joda.time.format.DateTimeFormat;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import at.ac.tuwien.dse.fairsurgeries.domain.OPSlot;
+import at.ac.tuwien.dse.fairsurgeries.domain.OPSlotStatus;
 import at.ac.tuwien.dse.fairsurgeries.domain.SurgeryType;
 import at.ac.tuwien.dse.fairsurgeries.general.Constants;
 import at.ac.tuwien.dse.fairsurgeries.service.DoctorService;
@@ -39,27 +42,33 @@ public class ActorAdminController {
 	public String listSlots(Model uiModel) {
 		logEntryService.log(Constants.Component.Frontend.toString(), "Starting ActorAdminController . listSlots()");
 
-		this.setupModel(uiModel, new OPSlot());
+		this.setupModel(uiModel, new OPSlot(), null);
 
 		return "actors/admin/slots";
 	}
 
 	@RequestMapping(value = "/slots", method = RequestMethod.POST, produces = "text/html")
-	public String listFilteredSlots(@ModelAttribute OPSlot opSlot, Model uiModel) {
+	public String listFilteredSlots(@ModelAttribute OPSlot opSlot, Model uiModel, ServletRequest request) {
 		logEntryService.log(Constants.Component.Frontend.toString(), "Starting ActorAdminController . listFilteredSlots() example=" + opSlot);
 
-		this.setupModel(uiModel, opSlot);
+		String status = request.getParameter("status");
+		if(status == null || status.isEmpty())
+			this.setupModel(uiModel, opSlot, null);
+		else
+			this.setupModel(uiModel, opSlot, OPSlotStatus.valueOf(status));
 
 		return "actors/admin/slots";
 	}
 
-	private void setupModel(Model uiModel, OPSlot slotFilter) {
+	private void setupModel(Model uiModel, OPSlot slotFilter, OPSlotStatus status) {
 		uiModel.addAttribute("opSlots", opSlotService.findByExample(slotFilter));
 		uiModel.addAttribute("surgeryTypes", Arrays.asList(SurgeryType.values()));
+		uiModel.addAttribute("statusList", Arrays.asList(OPSlotStatus.values()));
 		uiModel.addAttribute("hospitals", hospitalService.findAllHospitals());
 		uiModel.addAttribute("doctors", doctorService.findAllDoctors());
 		uiModel.addAttribute("patients", patientService.findAllPatients());
 		uiModel.addAttribute("dateFormat", DateTimeFormat.patternForStyle("MS", LocaleContextHolder.getLocale()));
 		uiModel.addAttribute("slotFilter", slotFilter);
+		uiModel.addAttribute("status", status);
 	}
 }
