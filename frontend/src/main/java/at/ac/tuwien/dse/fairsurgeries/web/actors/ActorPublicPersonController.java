f397a31d78335410b2a31b9d5b13d77ca16019b7
Johannes Kurz <blackandcold@e214-226.eduroam.tuwien.ac.at>
2012-06-15 14:51:10 +0200

    working rest request

--- frontend/src/main/java/at/ac/tuwien/dse/fairsurgeries/web/actors/ActorPublicPersonController.java
+++ frontend/src/main/java/at/ac/tuwien/dse/fairsurgeries/web/actors/ActorPublicPersonController.java
@@ -2,6 +2,7 @@ package at.ac.tuwien.dse.fairsurgeries.web.actors;
 
 import java.util.Arrays;
 import java.util.Enumeration;
+import java.util.List;
 
 import javax.servlet.ServletRequest;
 
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import at.ac.tuwien.dse.fairsurgeries.domain.Doctor;
 import at.ac.tuwien.dse.fairsurgeries.domain.OPSlot;
 import at.ac.tuwien.dse.fairsurgeries.domain.SurgeryType;
 import at.ac.tuwien.dse.fairsurgeries.general.Constants;
@@ -23,6 +25,12 @@ import at.ac.tuwien.dse.fairsurgeries.service.HospitalService;
 import at.ac.tuwien.dse.fairsurgeries.service.LogEntryService;
 import at.ac.tuwien.dse.fairsurgeries.service.OPSlotService;
 import at.ac.tuwien.dse.fairsurgeries.domain.OPSlotStatus;
+
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 
 @Controller
 @RequestMapping("/actors/public")
@@ -67,4 +75,26 @@ public class ActorPublicPersonController {
 		uiModel.addAttribute("slotFilter", slotFilter);
 		uiModel.addAttribute("status", status);
 	}
+	
+	/*
+	 * curl -i -X GET -H "Content-Type: application/json" -H "Accept: application/json" http://dse_frontend.cloudfoundry.com/actors/public/slots_json?test=FUUU
+	 * 
+	 */
+	
+	@RequestMapping(value = "/slots_json", method = RequestMethod.GET, headers = "Accept=application/json")
+    @ResponseBody
+    public ResponseEntity<String> showJSON(@RequestParam("test") String param, Model model) {
+    	logEntryService.log(Constants.Component.Frontend.toString(), "Starting ActorPublicPersonController . showJSON() param: " + param);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json; charset=utf-8");
+
+        //List<Doctor> doctorsCollection = doctorService.findAllDoctors();
+        String json = "{\"Parameter\": "+param+"}";
+        
+        return new ResponseEntity<String>(json, headers, HttpStatus.OK);
+      
+        // Some Code Missing Here  
+        //return new ResponseEntity<string>(headers, HttpStatus.NOT_FOUND);
+    }
 }
